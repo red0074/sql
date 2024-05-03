ac76cdda1a35991db1d313f725b958d21a775c55
red0074 <pdinesh122003@gmail.com>
2024-05-03 21:05:49 +0530

    select in detail and examples with where clause , where with operators

---
+++ select_where.sql
@@ -0,0 +1,15 @@
+use college;
+select name from student;
+
+select city from student;
+
+select name,marks from student;
+
+select distinct city from student;
+
+select * from student where marks >80;
+
+select * from student where city ="mumbai";
+
+select * from student where
+marks >80 and city ="delhi";
