52497468b76641a10e45ac42f697da8b75350e31
red0074 <pdinesh122003@gmail.com>
2024-05-03 20:29:32 +0530

    creating database,table and inserting data

---
+++ practice_q1.sql
@@ -0,0 +1,16 @@
+create database xyz;
+use xyz;
+create table employee(
+	id int primary key,
+    name varchar(50),
+    salary int not null
+    );
+    
+insert into employee
+(id,name,salary)
+ values
+ (1,"adam",25000),
+ (2,"bob",30000),
+ (3,"casey",40000);
+ 
+ select * from employee;
